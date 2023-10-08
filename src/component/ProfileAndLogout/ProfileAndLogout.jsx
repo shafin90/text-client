da8731cbada9a9a363f007c8c657e6fbba4c8b12
shafin90 <mashrafiahnam1@gmail.com>
2023-10-07 22:07:52 -0700

    push

--- src/component/ProfileAndLogout/ProfileAndLogout.jsx
+++ src/component/ProfileAndLogout/ProfileAndLogout.jsx
@@ -3,9 +3,11 @@ import { useContext } from 'react';
 import { BiLogOut } from 'react-icons/bi'
 import { authContext } from '../AuthProvider/AuthProvider';
 import { useNavigate } from 'react-router-dom';
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react';
 
 const ProfileAndLogout = () => {
     const { handleLogOut, logOutConfirm, loggedInUserInfo } = useContext(authContext);
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     // Initialize useNavigate Hook.
     const navigate = useNavigate();
@@ -19,7 +21,7 @@ const ProfileAndLogout = () => {
         <div className=' flex justify-between items-center w-10/12 mx-auto  relative bottom-4 '>
             {/* Profile image of loggedIN user and user's name */}
             <div className=' flex justify-between items-center'>
-                <img src={loggedInUserInfo?.img} alt="profile-pic" className=" w-9 h-9 rounded-full  border-2 border-white" />
+                <img onClick={onOpen} src={loggedInUserInfo?.img} alt="profile-pic" className=" w-9 h-9 rounded-full  border-2 border-white" />
                 <p className=' text-slate-300 ms-3'>{loggedInUserInfo?.name}</p>
             </div>
 
@@ -27,6 +29,28 @@ const ProfileAndLogout = () => {
             <button className=' hover:bg-red-800 p-2 rounded-md' onClick={handleLogOut}>
                 <BiLogOut className=' text-slate-400 text-2xl'></BiLogOut>
             </button>
+
+
+            <Modal  blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+                
+                <ModalContent className=" bg-gray-700 text-slate-300 w-screen h-screen flex justify-center items-center">
+                    
+                    
+                    <ModalBody>
+                        <h1 className=' text-4xl font-bold text-center'>{loggedInUserInfo?.name}</h1>
+                        <h2 className=' text-xl font-semibold text-center mb-3'>{loggedInUserInfo?.email}</h2>
+                        <img src={loggedInUserInfo?.img} className=' w-40 h-40 rounded-full border-4 border-slate-300 ' alt="" />
+                        
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button className=' px-4 py-2 bg-slate-300 text-slate-800 rounded-md mt-3' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                    
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </div>
     );
 };
--- src/component/ProfileAndMenu/ProfileAndMenu.jsx
+++ src/component/ProfileAndMenu/ProfileAndMenu.jsx
@@ -1,19 +1,66 @@
 // This component contains profile picture and a menu-bar
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { authContext } from '../AuthProvider/AuthProvider';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Button,
+    Input,
+} from '@chakra-ui/react'
 
 const ProfileAndMenu = () => {
     const { to } = useContext(authContext);//Retrieve user's information to whom user send message
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
 
     return (
-        <div className={to==null?' hidden':' flex justify-between items-center w-full h-24 px-10'}>
+        <div className={to == null ? ' hidden' : ' flex justify-between items-center w-full h-24 px-10'}>
             <div className=' flex justify-between items-center'>
                 <img src={to?.img} alt="profile" className="rounded-full border-2 border-black w-9 h-9" />
                 <p className=' ms-3 font-bold'>{to?.name}</p>
             </div>
-            <GiHamburgerMenu className=' cursor-pointer text-2xl'></GiHamburgerMenu>
+            <GiHamburgerMenu ref={btnRef} colorScheme='teal' onClick={onOpen} className=' cursor-pointer text-2xl'></GiHamburgerMenu>
+
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent className=' w-screen h-screen bg-gray-800 text-slate-300 flex justify-center items-center'>
+
+                    <DrawerHeader>
+                        <h1 className=' text-5xl font-bold mb-2'>{to?.name}</h1>
+                        <h3 className=' text-slate-300 mb-5 text-center'>{to.email}</h3>
+                    </DrawerHeader>
+
+                    <DrawerBody>
+                        <img src={to?.img} className=' w-52 h-52 rounded-full border-4 border-slate-300 mb-4' alt="" />
+
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                        <Button className=" bg-slate-300 text-slate-800 rounded-md px-6 py-2" mr={3} onClick={onClose}>
+                            Cancel
+                        </Button>
+
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+
+
+
+
         </div>
+
     );
 };
 
