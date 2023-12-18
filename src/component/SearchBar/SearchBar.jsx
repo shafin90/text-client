1eff18096df33070b4bde655ab50332c08bae1e5
shafin90 <mashrafiahnam1@gmail.com>
2023-12-18 06:42:27 -0800

    push

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -8,10 +8,6 @@ import { useStatStyles } from '@chakra-ui/react';
 const SearchBar = () => {
     const {  setSearchInput } = useContext(authContext);
     // State declaration for this component========================
-    
-
-   
-
   
     return (
         <div className="flex items-center  bg-slate-200  w-10/12 p-2 rounded-md mt-4 mx-auto">
